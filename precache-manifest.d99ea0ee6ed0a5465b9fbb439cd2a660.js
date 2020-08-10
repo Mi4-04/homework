a1b1807aa7a42fcc741f15085aec93fb4da13e5e
Mi4-04 <zakharovm2001@mail.ru>
2020-08-10 11:13:39 +0300

    Updates

---
+++ precache-manifest.d99ea0ee6ed0a5465b9fbb439cd2a660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb74f5252422747e98d8107876fcec6a",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "edc72f99cbdc84bd3ef0",
+    "url": "/homework/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bd5dbf097880348e65b8",
+    "url": "/homework/static/js/2.cefa576f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework/static/js/2.cefa576f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc72f99cbdc84bd3ef0",
+    "url": "/homework/static/js/main.67e2cba7.chunk.js"
+  },
+  {
+    "revision": "0d892f79005293ca720e",
+    "url": "/homework/static/js/runtime-main.8777bf7b.js"
+  }
+]);
