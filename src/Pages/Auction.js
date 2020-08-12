900cc5c4ad2d11be18601e693f0481e9e826cb89
Misha <co6a4ken01@mail.ru>
2020-08-12 14:12:01 +0300

    homePage, navigation
    Изменения на 13.08.2020

---
+++ src/Pages/Auction.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Auction = () => {
+  return (
+    <div>
+      <h1>Аукцион</h1>
+    </div>
+  );
+};
---
+++ src/Pages/orders.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Orders = () => {
+  return (
+    <div>
+      <h1>Список заказов</h1>
+    </div>
+  );
+};
